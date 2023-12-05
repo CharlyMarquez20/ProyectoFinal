76d828662225bd810c0bb1a72e49a291ebaf997f
Sara Yatzil <saracarrillodiaz13@outlook.com>
2023-12-04 18:59:07 -0600

    Páginas 'Acerca de' e 'Inicio'

---
+++ acercade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content=" classth=device- classth, initial-scale=1.0">
+    <link rel="stylesheet" href="css/acercade.css">
+    <link rel="stylesheet" href="css/general.css">
+    <link rel="shortcut icon" type="image/x-icon" href="images/logo/Logo.png">
+    <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
+
+    <title>Acerca de</title>
+</head>
+<body>
+    <div class="section1">
+        <div class="info1">
+            <p>Bienveidos a OASIS, el lugar que te ofrece una experiencia donde cada par cuenta una historia y cada cliente encuentra su paraíso personal en cada paso que da. <br>¡Bienvenidos a nuestro oasis de estilo y confort!</p>
+        </div>
+        <div class="img1">
+            <img src="images/acercade/img1.jpg" alt="Imagen no encontrada" width="600">
+        </div>
+    </div>
+    
+    <div class="section2">
+        <div class="title">
+            <h2>ACERCA  DE</h2>
+        </div>
+        <div class="info2">
+            <h4>VISIÓN</h4>
+            <p>Ser reconocidos como el destino predilecto para aquellos que buscan no solo tenis, sino una conexión auténtica con su estilo y pasión. Queremos ser la inspiración que impulsa a cada persona a encontrar su propio paraíso en cada par de tenis que eligen.</p>
+            <h4>MISIÓN</h4>
+            <p>En OASIS, nos esforzamos por proporcionar la mejor selección de tenis de alta calidad, fusionando moda, funcionalidad y comodidad. Buscamos constantemente las últimas tendencias y marcas más exclusivas para garantizar que nuestros clientes encuentren la joya perfecta para sus pies. A través de un servicio personalizado y una plataforma de compra intuitiva, pretendemos hacer que la búsqueda y adquisición de tenis sea una experiencia emocionante y satisfactoria.</p>
+        </div>
+        <div class="title2">
+            <h3>TITLE</h3>
+        </div>
+    </div>
+    
+    <div class="section3">
+        <div class="img2">
+          <img src="https://images.unsplash.com/photo-1542280756-74b2f55e73ab?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" width="600">
+        </div>
+        <div class="info3">
+            <h4>OBJETIVO</h4>
+            <p>Nuestro objetivo es convertirnos en el referente líder en la venta de tenis en línea, destacando por la calidad de nuestros productos, la variedad de marcas exclusivas y la excelencia en el servicio al cliente. Buscamos establecer una comunidad apasionada de amantes de los tenis que encuentren en OASIS su destino predilecto, donde cada par adquerido se convierte en una fuente de felicidad y autenticidad.</p>
+        </div>
+    </div>
+
+</body>
+</html>
---
+++ inicio.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" type="image/x-icon" href="images/logo/Logo.png">
+    <link rel="stylesheet" href="css/inicio.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <title>OASIS</title>
+</head>
+<body>
+    <div class="carrusel">
+        <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="images/inicio/1.jpg" class="d-block w-100" alt="..." height="auto">
+                </div>
+                <div class="carousel-item">
+                    <img src="images/inicio/2.jpg" class="d-block w-100" alt="..." height="auto">
+                </div>
+                <div class="carousel-item">
+                    <img src="images/inicio/3.jpg" class="d-block w-100" alt="..." height="auto">
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+    </div>
+    
+
+    <div class="section2">
+        <h2>Destacados</h2>
+        <img class="grow" id="img1" src="https://images.unsplash.com/photo-1626379616459-b2ce1d9decbc?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" width="400">
+        <img class="img2" src="https://images.unsplash.com/photo-1595950653106-6c9ebd614d3a?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" width="200">
+        <p id="p1">Producto</p>
+        <img class="img3" src="https://images.unsplash.com/photo-1600185652960-c9d8869d015c?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" width="200"
+        height="200">
+        <p id="p2">Colección urbana -></p>
+    </div>
+    <div class="section3">
+        <img src="https://brand.assets.adidas.com/image/upload/f_auto,q_auto,fl_lossy/if_w_gt_1920,w_1920/enPH/Images/ph-plp-mh-d_tcm184-1034948.jpg" alt="">
+    </div>
+    <div class="section4">
+        <h2>Colecciones</h2>
+        <img class="img1" src="images/inicio/colecc1.png" alt="" width="200" height="200">
+        <p id="col1">Mujer</p>
+        <img id="img2" src="images/inicio/colecc2.png" alt="" width="200" height="200">
+        <p id="col2">Hombre</p>
+        <img id="img3" src="images/inicio/colecc3.png" alt="" width="200" height="200">
+        <p id="col3">Niños (Próximamente)</p>
+    </div>
+    <div class="section6">
+        <h2>OASIS x Adidas x ITZY</h2>
+        <iframe width="900" height="480" src="https://www.youtube.com/embed/549UtQ_jZpE" title="adidas Running l ULTRABOOST 22 l ITZY에게 한계란 없지" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    </div>
+    <div class="section7">
+        <small>REVIEWS</small>
+        <h2>TESTIMONIOS</h2>
+        <div id="test1">
+            <h4 style="text-align:center;">Demasiado cómodo</h4>
+            <p>"Calidad premium y nada con lo que no se pueda combinar."<br>- Anne L.</p>
+        </div>
+        <div id="test2">
+            <h4 style="text-align:center;">Versátil</h4>
+            <p>"Este par es perfecto para mi día a día, podría usarlos hasta para dormir."<br>- Jake K.</p>
+        </div>
+        <div id="test3">
+            <h4 style="text-align:center;">Elegante</h4>
+            <p>"Estoy completamente fascinado con la elegancia y comodidad de este par."<br>- John S.</p>
+        </div>
+    </div>
+    <div class="section8">
+        <h2>Ubica nuestra tienda física</h2>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d59224.96984886072!2d-102.38680647832025!3d21.912989300000007!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8429ef1da1ab338d%3A0x89a0246637c42ddb!2sUniversidad%20Autónoma%20de%20Aguascalientes!5e0!3m2!1ses-419!2smx!4v1701707187799!5m2!1ses-419!2smx" width="100%" height="400" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+</body>
+</html>
