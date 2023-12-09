4ec3b366bac843199156691f3c58673d7efdde7a
Valeria <ppollux.velas@gmail.com>
2023-12-09 16:28:02 -0600

    si

---
+++ formularios/agregar_prod.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tu Tienda</title>
+    <link rel="stylesheet" href="estilos.css"> <!-- Agrega tus estilos aquí -->
+</head>
+<body>
+    <form action="datos_personales.php" method="post">
+        <label for="producto">Producto:</label>
+        <input type="text" id="producto" name="producto" required>
+
+        <label for="cantidad">Cantidad:</label>
+        <input type="number" id="cantidad" name="cantidad" required>
+
+        <label for="precio">Precio:</label>
+        <input type="number" id="precio" name="precio" required>
+
+        <!-- Agrega un campo para la imagen del producto (puedes usar un input de tipo "file") -->
+
+        <button type="submit">Agregar al carrito</button>
+    </form>
+</body>
+</html>
